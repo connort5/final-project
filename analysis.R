b0457c6eabec56eedd23a7d34e7881b70b8e7399
Sangwon Yoon <syoon1998@outlook.com>
2019-02-23 19:06:17 -0800

    Filter excess deaths data

--- analysis.R
+++ analysis.R
@@ -1,2 +1,16 @@
 # Any data analysis will be written in this R script file and sourced into the R markdown file. 
 # This file is named "analysis.R"
+library(dplyr)
+
+# Temporary assignment for testing - remove before submitting
+# Both are dataframes
+drug_poisoning <- read.csv("data/drug_poisoning.csv", stringsAsFactors = FALSE)
+excess_deaths <- read.csv("data/excess_deaths.csv", stringsAsFactors = FALSE)
+
+filtered_excess_deaths <- excess_deaths %>%
+  filter(Locality == "All", Age.Range == "0-84", Benchmark == "Floating") %>%
+  select(-State.FIPS.Code, -HHS.Region, -Locality, -Age.Range, -Benchmark) %>%
+  group_by(State) %>%
+  filter(Percent.Potentially.Excess.Deaths == max(Percent.Potentially.Excess.Deaths))
+
+View(filtered_excess_deaths)
