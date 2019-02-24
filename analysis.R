1d34b7a4ea457340277225539adddbc6fc7c074a
Sangwon Yoon <syoon1998@outlook.com>
2019-02-24 10:07:32 -0800

    Move filters to index.Rmd as functions

--- analysis.R
+++ analysis.R
@@ -7,14 +7,18 @@ library(dplyr)
 drug_poisoning <- read.csv("data/drug_poisoning.csv", stringsAsFactors = FALSE)
 excess_deaths <- read.csv("data/excess_deaths.csv", stringsAsFactors = FALSE)
 
-filtered_excess_deaths <- excess_deaths %>%
+filter_excess_deaths <- function(df){
+  filtered_excess_deaths <- df %>%
   filter(Locality == "All", Age.Range == "0-84", Benchmark == "Floating") %>%
-  select(-State.FIPS.Code, -HHS.Region, -Locality, -Age.Range, -Benchmark) %>%
-  group_by(State)
+  select(-State.FIPS.Code, -HHS.Region, -Locality, -Age.Range, -Benchmark)
+  
+  filtered_excess_deaths
+}
 
-filtered_drug_poisoning <- drug_poisoning %>%
+filter_drug_poisoning <- function(df){
+  filtered_drug_poisoning <- df %>%
   filter(Sex == "Both Sexes", Age.Group == "All Ages", Race.and.Hispanic.Origin == "All Races-All Origins") %>%
   select(-Sex, -Age.Group, -Race.and.Hispanic.Origin)
-
-View(filtered_excess_deaths)
-View(filtered_drug_poisoning)
+  
+  filtered_drug_poisoning
+}
