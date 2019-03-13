0f28af84050a0faf0bfe22b0d591d8d2907d25d4
Esha <esha_gulati@hotmail.com>
2019-03-12 23:17:00 -0700

    updated fatal poisoning analysis

--- analysis.R
+++ analysis.R
@@ -194,5 +194,4 @@ fatal_poisoning_rate_ <- drug_poisoning %>% #2005 - 2015
     Sex == "Both Sexes",
     Race.and.Hispanic.Origin == "All Races-All Origins"
   )
-
 # Comparing to the rates of the five leading causes of death 
